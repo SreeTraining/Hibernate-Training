61a08605d2ec4cdcad67b929efba5b21978fa2e4
sreechoudary <sree.muppavarapu@gmail.com>
2014-03-04 01:02:57 +0530

    Adding Second Level Cache Example.

---
+++ HBSecondLevelCache/src/main/java/com/sree/training/StoreTest.java
@@ -0,0 +1,18 @@
+package com.sree.training;
+
+import org.hibernate.*;
+import org.hibernate.cfg.*;
+
+public class StoreTest {
+	public static void main(String[] args) {
+		Session session=new Configuration().configure("hibernate.cfg.xml").buildSessionFactory().openSession();
+		
+		Transaction tx=session.beginTransaction();
+		
+		session.persist(new Employee("Rahul",50000));
+		session.persist(new Employee("Ajay",70000));
+		
+		tx.commit();
+		session.close();
+	}
+}
